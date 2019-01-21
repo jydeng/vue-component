b9013706cfa62f7dcea3a2a42871ecbb10f79aee
Roy <529909869@qq.com>
2019-01-21 18:18:34 +0800

    init radio组件

---
+++ src/components/Radio/Radio.vue
@@ -0,0 +1,51 @@
+<template>
+  <label>
+    <span>
+      <input
+        type="radio"
+        :disabled="disabled"
+        :checked="currentValue"
+        @change="change"
+      >
+    </span>
+    <slot></slot>
+  </label>
+</template>
+<script>
+export default {
+  name: "iRadio",
+  props: {
+    label: {
+      type: [String, Number, Boolean]
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    value: {
+      type: [String, Number, Boolean],
+      default: false
+    },
+    trueValue: {
+      type: [String, Number, Boolean],
+      default: true
+    },
+    falseValue: {
+      type: [String, Number, Boolean],
+      default: false
+    }
+  },
+  data() {
+    return {
+      currentValue: this.value
+    };
+  },
+  methods: {
+    change(event) {
+      if (this.disabled) {
+        return false;
+      }
+    }
+  }
+};
+</script>
