fa96f99786918a1e587e0f5c2022e6812d28576c
Roy <529909869@qq.com>
2019-01-23 17:56:12 +0800

    新增Alert组件，新的封装方式

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import Alert from "./components/Alert/alert.js";
 
 Vue.config.productionTip = false;
+Vue.prototype.$Alert = Alert;
 
 new Vue({
   router,
